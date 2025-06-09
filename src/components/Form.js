790fce520df9548d0159a9e0c00c4335681f4669
Iashkina Adelina <yadelishka@gmail.com>
2025-06-09 10:55:45 +0300

    finish with the division to the components

---
+++ src/components/Form.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+export default function Form({ onAddItems }) {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(5);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+
+    onAddItems(newItem);
+
+    setDescription("");
+    setQuantity(1);
+  }
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your 😍 trip?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => {
+          console.log(e.target);
+          setQuantity(Number(e.target.value));
+        }}
+      >
+        {Array.from({ length: 20 }, (__, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => {
+          console.log(e.target);
+          setDescription(e.target.value);
+        }}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
